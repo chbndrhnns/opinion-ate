4b5fe199b74b17714e53edc14f1c577b4c85d33d
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-12-20 11:43:05 +0100

    (feat) Add RestaurantList and RestaurantScreen

---
+++ src/components/RestaurantScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>RestaurantScreen</h1>
+    <restaurant-list />
+  </div>
+</template>
+
+<script>
+import RestaurantList from './RestaurantList.vue';
+export default {
+  name: 'RestaurantScreen',
+  components: {
+    RestaurantList,
+  },
+};
+</script>
