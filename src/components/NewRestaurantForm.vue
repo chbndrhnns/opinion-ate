76396594f973d8a2e1d22e0b1870426ce17d11f0
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-12-23 23:37:36 +0100

    (feat) Add form for adding a new restaurant

---
+++ src/components/NewRestaurantForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <v-text-field
+      placeholder="Add restaurant"
+      filled
+      type="text"
+    ></v-text-field>
+    <v-btn color="primary" class="black--text">Add</v-btn>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'NewRestaurantForm',
+};
+</script>
