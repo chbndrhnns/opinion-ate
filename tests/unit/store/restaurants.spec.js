804123519bf71b584f80a5d1f644d753f9c1d805
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-12-23 21:23:19 +0100

    (feat) Add loading flag to restaurants store

--- tests/unit/store/restaurants.spec.js
+++ tests/unit/store/restaurants.spec.js
@@ -3,26 +3,35 @@ import {createLocalVue} from '@vue/test-utils';
 import restaurants from '@/store/restaurants';
 
 describe('restaurants', () => {
+  const records = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
+
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
-  describe('load action', () => {
-    it('stores the restaurant', async () => {
-      const records = [
-        {id: 1, name: 'Sushi Place'},
-        {id: 2, name: 'Pizza Place'},
-      ];
+  let store;
 
-      const api = {loadRestaurants: () => Promise.resolve(records)};
-      const store = new Vuex.Store({
-        modules: {
-          restaurants: restaurants(api),
-        },
-      });
+  beforeEach(() => {
+    const api = {loadRestaurants: () => Promise.resolve(records)};
+    store = new Vuex.Store({
+      modules: {
+        restaurants: restaurants(api),
+      },
+    });
+    return store.dispatch('restaurants/load');
+  });
 
-      await store.dispatch('restaurants/load');
-
+  describe('when loading succeeds', () => {
+    it('stores the restaurant', () => {
       expect(store.state.restaurants.records).toEqual(records);
+    });
+  });
+
+  describe('while loading', () => {
+    it('sets a loading flag', () => {
+      expect(store.state.restaurants.loading).toEqual(true);
     });
   });
 });
