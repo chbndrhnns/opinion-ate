56babbd67f64074f8a508d5d9d76700d5f0fbd67
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-12-23 23:56:19 +0100

    (feat) Can submit form with restaurant name

--- src/components/NewRestaurantForm.vue
+++ src/components/NewRestaurantForm.vue
@@ -1,16 +1,39 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSave">
     <v-text-field
       placeholder="Add restaurant"
+      data-testid="new-restaurant-name-field"
+      v-model="name"
       filled
       type="text"
     ></v-text-field>
-    <v-btn color="primary" class="black--text">Add</v-btn>
+    <v-btn
+      color="primary"
+      type="submit"
+      class="black--text"
+      data-testid="new-restaurant-submit-button"
+      >Add</v-btn
+    >
   </form>
 </template>
 
 <script>
+import {mapActions} from 'vuex';
+
 export default {
   name: 'NewRestaurantForm',
+  data() {
+    return {
+      name: '',
+    };
+  },
+  methods: {
+    ...mapActions({
+      createRestaurant: 'restaurants/create',
+    }),
+    handleSave() {
+      this.createRestaurant(this.name);
+    },
+  },
 };
 </script>
