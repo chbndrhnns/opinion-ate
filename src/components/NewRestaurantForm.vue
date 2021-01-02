f2f68cf14346f6a929c42b6dda85198ab9c34adf
chbndrhnns <chbndrhnns@users.noreply.github.com>
2021-01-02 14:36:57 +0100

    (refactor) Use v-row/v-col

--- src/components/NewRestaurantForm.vue
+++ src/components/NewRestaurantForm.vue
@@ -14,20 +14,27 @@
       :icon="false"
       >The restaurant could not be saved. Please try again.</v-alert
     >
-    <v-text-field
-      placeholder="Add restaurant"
-      data-testid="new-restaurant-name-field"
-      v-model="name"
-      filled
-      type="text"
-    ></v-text-field>
-    <v-btn
-      color="primary"
-      type="submit"
-      class="black--text"
-      data-testid="new-restaurant-submit-button"
-      >Add</v-btn
-    >
+    <v-row>
+      <v-col cols="9">
+        <v-text-field
+          placeholder="Add restaurant"
+          data-testid="new-restaurant-name-field"
+          v-model="name"
+          filled
+          type="text"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="3">
+        <v-btn
+          block
+          color="primary"
+          type="submit"
+          class="black--text"
+          data-testid="new-restaurant-submit-button"
+          >Add</v-btn
+        >
+      </v-col>
+    </v-row>
   </form>
 </template>
 
